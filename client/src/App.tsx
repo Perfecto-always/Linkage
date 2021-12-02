9c2975b34d9d247c076dd1d129a3148f2cab194f
Perfecto-always <harsh24bararia@gmail.com>
2021-12-02 07:48:59 +0530

    Added SEO for the website

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,8 +1,16 @@
 import AllRoutes from "./Routes";
+import { Helmet } from "react-helmet";
 
 function App() {
   return (
     <>
+      <Helmet>
+        <meta
+          name='description'
+          content='Chat listen to music all at the same time'
+        />
+        <link rel='canonical' href='https://praline.netlify.app/chat' />
+      </Helmet>
       <AllRoutes />
     </>
   );
